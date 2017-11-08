7e2781e9f1094de11551a5d29ef6b3bb70251769
Willy0591 <pernez@lacroixrouge-brest.fr>
2017-11-08 09:05:48 +0100

    envoi d'une lat etd'une longi négative

--- clientudp.py
+++ clientudp.py
@@ -12,15 +12,27 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.sendto (trame,(ip , 5050))
 data,addr=sock.recvfrom(1024)
     
-lat1=float(ord (data[5])<<24)
-lat2=lat1+(ord (data[6])<<16)
-lat3=lat2+(ord(data[7])<<8)
-lat4=lat3+(ord (data[8]))
-divis = (lat4/1000000)
+lat= ord(data[4])<<24 | ord(data[5])<<16 | ord(data[6])<<8 | ord(data[7])
+lat_f = float(lat)/10000000
+
+if ord(data[4]) > 127:
+    lat = (~lat) &0xFFFFFFFF
+    lat = (lat+1)*-1 
+lat_f = float(lat)/10000000
+
+longe=ord(data[8])<<24  | ord(data[9])<<16 | ord(data[10])<<8 | ord(data[11])
+
+
+if ord(data[8]) > 127:
+    longe = (~longe) &0xFFFFFFFF
+    longe = (longe+1)*-1 
+longe_f = float(longe)/10000000
 
 print"ID=", ord(data[0])
 print"Taille=", ord(data[1])
 print "Vvent=", ord(data[2])
 print "Dvent=", ord(data[3])
-print "Gite=" , ord(data[4])
-print "latitude",divis
+print "Gite=" , ord(data[12])
+print "latitude=",lat_f
+print "longitude=",longe_f
+
